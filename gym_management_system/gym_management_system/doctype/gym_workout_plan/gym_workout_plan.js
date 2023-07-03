970b04a5480b91ab556e9cbc85de0d31d567ad66
Sandeep Kakde <sandeep@agkiya.com>
2023-07-04 00:11:24 +0530

    Adding DocTypes

---
+++ gym_management_system/gym_management_system/doctype/gym_workout_plan/gym_workout_plan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Sandeep Kakde and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Gym Workout Plan", {
+// 	refresh(frm) {
+
+// 	},
+// });
