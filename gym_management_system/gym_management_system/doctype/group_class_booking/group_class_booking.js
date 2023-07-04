3e5c78de5fd422d59d37a9634e91e13112a5fd8e
Sandeep Kakde <sandeep@agkiya.com>
2023-07-04 13:36:14 +0530

    feat: adding new doctypes & roles

---
+++ gym_management_system/gym_management_system/doctype/group_class_booking/group_class_booking.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Sandeep Kakde and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Group Class Booking", {
+// 	refresh(frm) {
+
+// 	},
+// });
