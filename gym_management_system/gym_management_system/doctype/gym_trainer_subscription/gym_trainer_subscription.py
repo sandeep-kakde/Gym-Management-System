d6c7111a665d0fa47d1cf9afdbf1b9eb0584057e
Sandeep Kakde <sandeep@agkiya.com>
2023-07-04 22:49:17 +0530

    feat: Adding duplicate entry check in Gym Trainer Subscription

--- gym_management_system/gym_management_system/doctype/gym_trainer_subscription/gym_trainer_subscription.py
+++ gym_management_system/gym_management_system/doctype/gym_trainer_subscription/gym_trainer_subscription.py
@@ -7,9 +7,12 @@ from frappe.model.docstatus import DocStatus
 
 
 class GymTrainerSubscription(Document):
+    def before_save(self):
+        self.check_duplicate_entry()
+
     def on_submit(self):
         self.update_trainer_ratings
-    
+
     def on_cancel(self):
         self.update_trainer_ratings()
 
@@ -24,5 +27,38 @@ class GymTrainerSubscription(Document):
             total_rating += rating
 
         frappe.db.set_value(
-            "Gym Trainer", self.trainer, "ratings", total_rating / len(trainer_ratings)
+            "Gym Trainer", self.trainer, "ratings", total_rating / (len(trainer_ratings) or 1)
         )
+
+    def check_duplicate_entry(self):
+        filters = [
+            ["member", "=", self.member],
+            ["docstatus", "=", DocStatus.submitted()],
+        ]
+        or_filters = [
+            [
+                "subscription_start_date",
+                "Between",
+                [self.subscription_start_date, self.subscription_end_date],
+            ],
+            [
+                "subscription_end_date",
+                "Between",
+                [self.subscription_start_date, self.subscription_end_date],
+            ],
+        ]
+
+        exists = frappe.db.get_all(
+            "Gym Trainer Subscription",
+            filters=filters,
+            or_filters=or_filters,
+            pluck="name",
+        )
+
+        if exists:
+            duplicate_membership_link = frappe.utils.get_link_to_form(
+                "Gym Trainer Subscription", exists[0]
+            )
+            frappe.throw(
+                f"Another active Trainer Subscription exists for the user with name {duplicate_membership_link}"
+            )
