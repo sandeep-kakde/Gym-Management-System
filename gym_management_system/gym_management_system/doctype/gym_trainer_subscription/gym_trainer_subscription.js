95e461e9f5260c81cde0e18e494518ed590a91fe
Sandeep Kakde <sandeep@agkiya.com>
2023-07-05 00:58:40 +0530

    feat: Adding Client script for restricting non admin users to see only there name in Gym Trainer Subscription

--- gym_management_system/gym_management_system/doctype/gym_trainer_subscription/gym_trainer_subscription.js
+++ gym_management_system/gym_management_system/doctype/gym_trainer_subscription/gym_trainer_subscription.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2023, Sandeep Kakde and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Gym Trainer Subscription", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Gym Trainer Subscription", {
+	refresh: async function(frm) {
+		if (!frappe.user_roles.includes("Gym Admin")) {
+			memberDocName = (await frappe.db.get_value("Gym Member", {"email": frappe.session.user}, "name")).message.name
+			frm.set_query("member", function () {
+				return {
+					filters: {
+						name: memberDocName,
+					},
+				};
+			});
+		}
+	}
+});
