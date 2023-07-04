f938907e9531f8a83b75ddabdfba86d705de5464
Sandeep Kakde <sandeep@agkiya.com>
2023-07-04 23:37:24 +0530

    test: Adding unit test for mandatory check in Gym Member

--- gym_management_system/gym_management_system/doctype/gym_member/test_gym_member.py
+++ gym_management_system/gym_management_system/doctype/gym_member/test_gym_member.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2023, Sandeep Kakde and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
+from frappe import MandatoryError
 
 
 class TestGymMember(FrappeTestCase):
-	pass
+    def test_mandatory(self):
+        gym_member = frappe.get_doc({
+            "doctype": "Gym Member"
+        })
+
+        self.assertRaises(MandatoryError, gym_member.insert)
